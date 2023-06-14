c7b3ca1215eb30dbbd0aef89028725a8d716f7ff
unknown <bogdan.lyamzin.d@gmail.com>
2023-06-14 19:45:59 +0300

    fix

---
+++ lesson-9/project-backend/routes/api/movies-routes.js
@@ -0,0 +1,27 @@
+const express = require("express");
+
+const moviesController = require("../../controllers/movies-controller");
+
+const schemas = require("../../schemas/movies");
+
+const {validateBody} = require("../../decorators");
+
+const {isValidId, authenticate} = require("../../middlewares")
+
+const router = express.Router();
+
+router.use(authenticate);
+
+router.get("/", moviesController.getAllMovies);
+
+router.get("/:id", isValidId, moviesController.getMovieById);
+
+router.post("/", validateBody(schemas.movieAddSchema), moviesController.addMovie);
+
+router.put("/:id", isValidId, validateBody(schemas.movieAddSchema), moviesController.updateMovieById);
+
+router.patch("/:id/favorite", isValidId, validateBody(schemas.movieUpdateFavoriteSchema), moviesController.updateFavorite);
+
+router.delete("/:id", isValidId, moviesController.deleteMovieById)
+
+module.exports = router;
